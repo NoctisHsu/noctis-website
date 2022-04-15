20ebc5d479a75dd772e09228b6983be5a6bccbd8
Noctis Hsu <noctishsu@91app.com>
2022-04-15 14:08:09 +0800

    feat: add packages

---
+++ packages/core/components/ErrorPage/500.tsx
@@ -0,0 +1,11 @@
+import { ErrorTemplate } from "./Template";
+
+export function ServerError() {
+  return (
+    <ErrorTemplate
+      errorTitle="500"
+      paragraph="Somethin' was wrong."
+      anchorMessage="Go to main"
+    />
+  );
+}
